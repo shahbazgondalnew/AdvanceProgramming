aa445d645c43f73c70cea99dc1a62c0bc3b91e2a
Shahbaz Gondal <shahbaz.gondal588@gmail.com>
2023-11-17 00:01:43 +0500

    login working

--- APproject/MainWindow.xaml.cs
+++ APproject/MainWindow.xaml.cs
@@ -1,27 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-
-
-
+using System.Data.SqlClient;
 using System.Windows;
 
 namespace APproject
 {
     public partial class MainWindow : Window
     {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True";
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,17 +19,38 @@ namespace APproject
             string username = txtUsername.Text;
             string password = txtPassword.Password;
 
-          
-
-            if (username == "admin" && password == "admin")
+            // Check user credentials against the database
+            if (AuthenticateUser(username, password))
             {
                 MessageBox.Show("Login successful!");
+                mainScreen mainMenu = new mainScreen();
+                mainMenu.Show();
+                this.Close();
+
             }
             else
             {
                 MessageBox.Show("Login failed. Please check your username and password.");
             }
         }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT COUNT(*) FROM userauth WHERE email = @username AND password = @password";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    int count = (int)command.ExecuteScalar();
+
+                    return count > 0;
+                }
+            }
+        }
     }
 }
-
